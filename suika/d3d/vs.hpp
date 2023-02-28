86f7e487653e0ab1a3ea79f52bab76e322ce9d22
ognknt <k661301@kansai-u.ac.jp>
2023-03-01 01:44:44 +0900

    vs

--- suika/d3d/vs.hpp
+++ suika/d3d/vs.hpp
@@ -1,1 +1,11 @@
 #pragma once
+#include <Windows.h>
+//https://qiita.com/ousttrue/items/8eabcd171fe00c674213
+namespace suika {
+	namespace d3d {
+		struct vertex_shader {
+			void create(const BYTE* input, size_t size);
+			void set();
+		};
+	}
+}

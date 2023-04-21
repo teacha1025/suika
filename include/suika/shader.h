2117089baadaea44f7efce12748bc7294f9abc22
teacha1025 <k661301@kansai-u.ac.jp>
2023-04-21 09:37:41 +0900

    あ

--- include/suika/shader.h
+++ include/suika/shader.h
@@ -1,7 +1,12 @@
 #pragma once
-
+#include "def.h"
 #include "string.h"
 namespace suika {
+	const string SHAPE_VERTEX = "shape";
+	const string SHAPE_PIXEL  = "shape";
+	const string TEXTURE_VERTEX = "texture";
+	const string TEXTURE_PIXEL  = "texture";
+
 	void add_vs(const string& input, const string& name);
 	void add_vs(const void* input, size_t size, const string& name);
 	void add_ps(const string& input, const string& name);

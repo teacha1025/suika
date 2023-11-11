89038d8b79234f7927b4e6c5ddca87640ae73440
ognknt <k661301@kansai-u.ac.jp>
2023-11-12 03:02:01 +0900

    update to v0.0.2 #4,#5,#16

--- docs/html/navtreeindex1.js
+++ docs/html/navtreeindex1.js
@@ -67,7 +67,7 @@ var NAVTREEINDEX1 =
 "index.html":[],
 "index.html":[0],
 "index.html#autotoc_md1":[0,0],
-"index.html#autotoc_md3":[0,1],
+"index.html#autotoc_md4":[0,1],
 "namespacemembers.html":[1,1,0,0],
 "namespacemembers.html":[1,1,0],
 "namespacemembers_b.html":[1,1,0,1],

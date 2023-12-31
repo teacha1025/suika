819d9baed787b10b4f55e3c77e9a057d4c805744
ognknt <k661301@kansai-u.ac.jp>
2023-12-31 19:04:35 +0900

    feature xaudio

---
+++ suika/d3d/bgm.cpp
@@ -0,0 +1,112 @@
+﻿#include "info.hpp"
+#include "xaudio.hpp"
+
+namespace suika {
+	namespace d3d {
+		namespace xaudio {
+			bgm::bgm(bgm&& s) noexcept {
+				if (is_init) {
+					if (is_init) {
+						s.stop();
+						_source_voice = std::move(s._source_voice);
+						_volume = s._volume;
+						_pan = s._pan;
+					}
+				}
+			}
+
+			bgm::~bgm() {
+				if (is_init) {
+					destroy();
+				}
+			}
+
+			void bgm::load(string path) {
+				_path = path;
+				if (is_init) {
+					_is_load = load_audio(path, _data);
+					if (!_is_load) {
+						log.error(std::format("音声:{}の読み込みに失敗", path));
+					}
+					if (_is_load) {
+						if (_source_voice) {
+							_source_voice->DestroyVoice();
+							_source_voice = nullptr;
+						}
+						_source_voice = nullptr;
+						auto er = p_xaudio->CreateSourceVoice(&_source_voice, wav_data[_path].wfx);
+						if (FAILED(er)) {
+							_source_voice = nullptr;
+							log_d3d.error("Failed to create source voice.");
+							log_d3d.result(er);
+							log.error(std::format("音声:{}の読み込みに失敗", path));
+							return;
+						}
+						if (_source_voice) {
+							_source_voice->GetVoiceDetails(&_details);
+						}
+					}
+				}
+			}
+
+			void bgm::mixer(submix_voice* mixer) {
+				if(is_init && _is_load && _source_voice) {
+					XAUDIO2_SEND_DESCRIPTOR send_descriptor = { .Flags = 0, .pOutputVoice = mixer->_submix_voice };
+					XAUDIO2_VOICE_SENDS send_list = { .SendCount = 1, .pSends = &send_descriptor };
+					_source_voice->SetOutputVoices(&send_list);
+				}
+			}
+
+			void bgm::volume(float volume) {
+				if(is_init && _is_load && _source_voice && _volume != volume) {
+					_volume = volume;
+					_source_voice->SetVolume(volume);
+				}
+			}
+
+			void bgm::pan(float pan) {
+				if (is_init && _is_load && _source_voice && _pan != pan) {
+					_pan = pan;
+					pan = (pan + 1.0f) / 4.0f * (float)M_PI;
+					panning(pan, _source_voice, _details.InputChannels, mastering_detail.InputChannels);
+				}
+			}
+
+			void bgm::play() {
+				if (is_init && _is_load && _source_voice) {
+					play_source(_source_voice, _path, _volume, _pan, _play_sample, _loop_data, _details);
+				}
+			}
+
+			bool bgm::is_playing() {
+				if (is_init && _is_load && _source_voice) {
+					XAUDIO2_VOICE_STATE state;
+					_source_voice->GetState(&state);
+					if (state.BuffersQueued > 0) {
+						return true;
+					}
+				}
+				return false;
+			}
+
+			void bgm::stop() {
+				if (is_init && _is_load && _source_voice) {
+					_source_voice->Stop();
+				}
+			}
+
+			void bgm::destroy() {
+				if (is_init && _is_load) {
+					stop();
+					if (_source_voice) {
+						_source_voice->DestroyVoice();
+						_source_voice = nullptr;
+					}
+					_path.clear();
+					_data.reset();
+					_is_load = false;
+				}
+			}
+		}
+	}
+}
---
+++ suika/d3d/xaduio.cpp
@@ -0,0 +1,205 @@
+﻿#include "info.hpp"
+#include "xaudio.hpp"
+
+namespace suika{
+    namespace d3d{
+        namespace xaudio{
+            Microsoft::WRL::ComPtr<IXAudio2> p_xaudio;
+            IXAudio2MasteringVoice* p_master_voice;
+            std::unordered_map<string, DirectX::WAVData> wav_data;
+            std::unordered_map<string, sound_effect> se_list;
+            std::unordered_map<string, bgm> bgm_list;
+            bool is_init = false;
+            XAUDIO2_VOICE_DETAILS mastering_detail;
+
+            void init(){
+                HRESULT hr;
+
+                UINT32 flags = 0;
+            #if _WIN32_WINNT < _WIN32_WINNT_WIN8 
+            #ifdef _DEBUG
+                flags |= XAUDIO2_DEBUG_ENGINE;
+            #endif
+            #endif
+
+                if (FAILED(hr = XAudio2Create(&p_xaudio, flags))) {
+                    is_init = false;
+					log_d3d.error("Failed to create XAudio instance.");
+					log_d3d.result(hr);
+					log.error("XAudio2の初期化に失敗");
+                    return;
+                }
+            #ifdef _DEBUG
+                XAUDIO2_DEBUG_CONFIGURATION debug{ 0 };
+                debug.TraceMask = XAUDIO2_LOG_ERRORS | XAUDIO2_LOG_WARNINGS;
+                debug.BreakMask = XAUDIO2_LOG_ERRORS;
+                p_xaudio->SetDebugConfiguration(&debug, 0);
+            #endif
+                if (FAILED(hr = p_xaudio->CreateMasteringVoice(&p_master_voice))) {
+                    is_init = false;
+					log_d3d.error("Failed to create mastering voice.");
+					log_d3d.result(hr);
+					log.error("XAudio2の初期化に失敗");
+                    return;
+                }
+                p_master_voice->GetVoiceDetails(&mastering_detail);
+
+                is_init = true;
+
+				d3d::log_d3d.info("XAudio2 initialized.");
+            }
+
+            void fin(){
+                if (is_init){
+                    for(auto& v : se_list) {
+                        v.second.stop();
+                    }  
+                    se_list.clear();
+                    for (auto& v : bgm_list) {
+                        v.second.destroy();
+                    }
+                    bgm_list.clear();
+                    if (p_master_voice != nullptr) {
+                        p_master_voice->DestroyVoice();
+                        p_master_voice = nullptr;
+                    }
+                    
+					is_init = false;
+
+					d3d::log_d3d.info("XAudio2 finalized.");
+                }
+            }
+
+#ifndef SPEAKER_STEREO
+#define SUIKA_DEF_SPEAKER
+#define SPEAKER_MONO             (SPEAKER_FRONT_CENTER)
+#define SPEAKER_STEREO           (SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT)
+#define SPEAKER_2POINT1          (SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT | SPEAKER_LOW_FREQUENCY)
+#define SPEAKER_SURROUND         (SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT | SPEAKER_FRONT_CENTER | SPEAKER_BACK_CENTER)
+#define SPEAKER_QUAD             (SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT | SPEAKER_BACK_LEFT | SPEAKER_BACK_RIGHT)
+#define SPEAKER_4POINT1          (SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT | SPEAKER_LOW_FREQUENCY | SPEAKER_BACK_LEFT | SPEAKER_BACK_RIGHT)
+#define SPEAKER_5POINT1          (SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT | SPEAKER_FRONT_CENTER | SPEAKER_LOW_FREQUENCY | SPEAKER_BACK_LEFT | SPEAKER_BACK_RIGHT)
+#define SPEAKER_7POINT1          (SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT | SPEAKER_FRONT_CENTER | SPEAKER_LOW_FREQUENCY | SPEAKER_BACK_LEFT | SPEAKER_BACK_RIGHT | SPEAKER_FRONT_LEFT_OF_CENTER | SPEAKER_FRONT_RIGHT_OF_CENTER)
+#define SPEAKER_5POINT1_SURROUND (SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT | SPEAKER_FRONT_CENTER | SPEAKER_LOW_FREQUENCY | SPEAKER_SIDE_LEFT | SPEAKER_SIDE_RIGHT)
+#define SPEAKER_7POINT1_SURROUND (SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT | SPEAKER_FRONT_CENTER | SPEAKER_LOW_FREQUENCY | SPEAKER_BACK_LEFT | SPEAKER_BACK_RIGHT | SPEAKER_SIDE_LEFT  | SPEAKER_SIDE_RIGHT)
+#endif
+
+			void panning(float pan, IXAudio2SourceVoice* source_voice, UINT32 in, UINT32 out) {
+				if (is_init && source_voice) {
+					std::vector<float> v(in * out);
+					float left = cosf(pan);
+					float right = sinf(pan);
+					DWORD mask;
+					auto er = p_master_voice->GetChannelMask(&mask);
+
+					if (FAILED(er)) {
+						log_d3d.error("Failed to SetOutputVoices.");
+						log_d3d.result(er);
+						return;
+					}
+
+					switch (mask) {
+						case SPEAKER_MONO:
+							v[0] = 1.0;
+							break;
+						case SPEAKER_STEREO:
+						case SPEAKER_2POINT1:
+						case SPEAKER_SURROUND:
+							v[0ull * in] = left;
+							v[1ull * in] = right;
+							break;
+						case SPEAKER_QUAD:
+							v[0ull * in] = v[2ull * in] = left;
+							v[0ull * in + 1] = v[2ull * in + 1] = right;
+							break;
+						case SPEAKER_4POINT1:
+							v[0ull * in] = v[3ull * in] = left;
+							v[0ull * in + 1] = v[3ull * in + 1] = right;
+							break;
+						case SPEAKER_5POINT1:
+						case SPEAKER_7POINT1:
+						case SPEAKER_5POINT1_SURROUND:
+							v[0ull * in] = v[4ull * in] = left;
+							v[0ull * in + 1] = v[4ull * in + 1] = right;
+							break;
+						case SPEAKER_7POINT1_SURROUND:
+							v[0ull * in] = v[4ull * in] = v[6ull * in] = left;
+							v[0ull * in + 1] = v[4ull * in + 1] = v[6ull * in + 1] = right;
+							break;
+					}
+					er = source_voice->SetOutputMatrix(nullptr, in, out, v.data());
+					if (FAILED(er)) {
+						log_d3d.error("Failed to SetOutputVoices.");
+						log_d3d.result(er);
+						log.error("音声のパン設定に失敗");
+						return;
+					}
+				}
+			}
+
+			void panning(float pan, IXAudio2SubmixVoice* source_voice, UINT32 in, UINT32 out) {
+				if (is_init && source_voice) {
+					std::vector<float> v(in * out);
+					float left = cosf(pan);
+					float right = sinf(pan);
+					DWORD mask;
+					auto er = p_master_voice->GetChannelMask(&mask);
+					if (FAILED(er)) {
+						log_d3d.error("Failed to GetChannelMask.");
+						log_d3d.result(er);
+						return;
+					}
+					switch (mask) {
+						case SPEAKER_MONO:
+							v[0] = 1.0;
+							break;
+						case SPEAKER_STEREO:
+						case SPEAKER_2POINT1:
+						case SPEAKER_SURROUND:
+							v[0ull * in] = left;
+							v[1ull * in] = right;
+							break;
+						case SPEAKER_QUAD:
+							v[0ull * in] = v[2ull * in] = left;
+							v[0ull * in + 1] = v[2ull * in + 1] = right;
+							break;
+						case SPEAKER_4POINT1:
+							v[0ull * in] = v[3ull * in] = left;
+							v[0ull * in + 1] = v[3ull * in + 1] = right;
+							break;
+						case SPEAKER_5POINT1:
+						case SPEAKER_7POINT1:
+						case SPEAKER_5POINT1_SURROUND:
+							v[0ull * in] = v[4ull * in] = left;
+							v[0ull * in + 1] = v[4ull * in + 1] = right;
+							break;
+						case SPEAKER_7POINT1_SURROUND:
+							v[0ull * in] = v[4ull * in] = v[6ull * in] = left;
+							v[0ull * in + 1] = v[4ull * in + 1] = v[6ull * in + 1] = right;
+							break;
+					}
+					er = source_voice->SetOutputMatrix(nullptr, in, out, v.data());
+					if (FAILED(er)) {
+						log_d3d.error("Failed to SetOutputVoices.");
+						log_d3d.result(er);
+						log.error("音声のパン設定に失敗");
+						return;
+					}
+				}
+			}
+#ifdef SUIKA_DEF_SPEAKER
+#undef SUIKA_DEF_SPEAKER
+#undef SPEAKER_MONO            
+#undef SPEAKER_STEREO          
+#undef SPEAKER_2POINT1         
+#undef SPEAKER_SURROUND        
+#undef SPEAKER_QUAD            
+#undef SPEAKER_4POINT1         
+#undef SPEAKER_5POINT1         
+#undef SPEAKER_7POINT1         
+#undef SPEAKER_5POINT1_SURROUND
+#undef SPEAKER_7POINT1_SURROUND
+#endif
+        }
+    }
+}
